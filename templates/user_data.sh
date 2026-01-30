ee7081f93d20e35dbb57bc9739fdeb5277eadf1b
Don Freeman <dbfreem2@yahoo.com>
2026-01-30 12:46:02 -0500

    disable source destination checks

--- templates/user_data.sh
+++ templates/user_data.sh
@@ -10,4 +10,12 @@ echo "cwagent_cfg_param_name=${TERRAFORM_CWAGENT_CFG_PARAM_NAME}" >> /etc/fck-na
 echo "gwlb_enabled=${TERRAFORM_GWLB_ENABLED}" >> /etc/fck-nat.conf
 echo "gwlb_health_check_port=${TERRAFORM_GWLB_HEALTH_CHECK_PORT}" >> /etc/fck-nat.conf
 
+# Disable source/dest check on the instance's ENI when GWLB is enabled
+if [ "${TERRAFORM_GWLB_ENABLED}" = "true" ]; then
+  TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
+  MAC=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/mac)
+  ENI_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/interface-id")
+  aws ec2 modify-network-interface-attribute --network-interface-id "$ENI_ID" --no-source-dest-check
+fi
+
 service fck-nat restart
